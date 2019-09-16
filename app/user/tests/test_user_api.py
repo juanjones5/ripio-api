4ab9c9c7c5c96b855119b8e890d3ab3906b3b5d1
juanjones5 <jonesjuanignacio@gmail.com>
2019-09-16 11:59:46 -0500

    switched to PyCharm and finished tag endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,7 +33,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
         self.assertTrue(user.check_password(payload['password']))
-        # self.assertNotIn('password', res.data)
+        self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
